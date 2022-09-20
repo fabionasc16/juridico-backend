e0d78186c6a7470b99be75e84ba84c410a58eb96
andrebatista2 <andrelucas.batista@outlook.com>
2022-09-20 08:16:33 -0400

    feat: Configurado Swagger UI

--- src/App.config.ts
+++ src/App.config.ts
@@ -6,6 +6,8 @@ import swaggerUi from 'swagger-ui-express';
 import 'express-async-errors';
 import YAML from 'yamljs';
 
+import { AppError } from './errors/AppError.class';
+
 const app = express();
 const swaggerDocument = YAML.load('./src/docs/swagger.yaml');
 
@@ -23,6 +25,24 @@ app.use(
   swaggerUi.setup(swaggerDocument, {
     customSiteTitle: 'Juridico API - Docs',
   }),
+);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        acknowledge: error.acknowledge,
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      acknowledge: false,
+      status: 'Internal Server Error',
+      message: `Error Log: ${error.message}`,
+    });
+  },
 );
 
 export { app };
