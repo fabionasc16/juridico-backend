8c7049bb6936c15073f4840672a5c9b217c07b48
Marcelo Pinheiro da Silva <marcelopds70@gmail.com>
2023-01-12 14:05:21 -0400

    recuperar senha

--- src/routes/Auth.routes.ts
+++ src/routes/Auth.routes.ts
@@ -8,6 +8,7 @@ const authService = new AuthService();
 authRoutes.post('/login', authService.authenticate);
 authRoutes.get('/profiles', authService.profiles);
 authRoutes.get('/user/exists/:cpf', authService.findUsuarioByCpf);
+authRoutes.post('/forgotpassword', authService.forgotPass);
 authRoutes.post('/resetpassword', authService.resetPassword);
 authRoutes.post('/cancelrequest', authService.cancelRequest);
 authRoutes.post('/verify', authService.verifyRole);
