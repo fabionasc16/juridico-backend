a94fa3d29524009a7e1d62b86d6ac7d5eb4cbc93
Isaac <isaac.bandeira@gmail.com>
2023-01-17 13:05:46 -0400

    initial

--- src/services/Auth.service.ts
+++ src/services/Auth.service.ts
@@ -157,7 +157,7 @@ export class AuthService {
             return await response.status(edit.status).json(edit.data);
           } catch (error) {
             return response.status(400).send({
-              message: 'Não foi possível Atualizar dados de usuário',
+              message: 'Não foi possível Cadastrar de usuário',
             });
           }
         } catch (error) {
